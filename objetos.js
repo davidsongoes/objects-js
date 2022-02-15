cc63a70708e85498485c2e1b07a635c7a974e265
Davidson Góes <davidsongoes@gmail.com>
2022-02-15 12:32:20 -0300

    feat(object): :sparkles: First Class - Initial object sctruture

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const cpf = ["12345678901", "98765432112", "12378945698"];
+
+const cliente = {
+  nome: "Davidson",
+  idade: 34,
+  cpf: "01598637169",
+  email: "davidsongoes@gmail.com",
+};
+console.log(cpf);
+console.log(cliente);
