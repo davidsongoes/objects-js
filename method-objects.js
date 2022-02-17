9111d451a0f1240af3e31696faa1160c8e07aa3e
Davidson Góes <davidsongoes@gmail.com>
2022-02-17 11:14:11 -0300

    feat(method-objects): :sparkles: Learning Object()

---
+++ method-objects.js
@@ -0,0 +1,9 @@
+const person = {
+  nome: "Davidson",
+  idade: 34,
+  email: "davidsongoes@gmail.com"
+};
+
+console.log(`Chaves: ${Object.keys(person)}`);
+console.log(`Valores: ${Object.values(person)}`);
+console.log(`Chaves,Valores: ${Object.entries(person)}`);
